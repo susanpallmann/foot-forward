5bfdbbbb550af17cfba5665417178483e6bfce42
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-05-13 16:35:26 -0400

    Update ui.js

--- ui.js
+++ ui.js
@@ -7,8 +7,15 @@ $(document).ready(function() {
   $(".checkbox").click(function() {
     if ($(this).attr("aria-checked") === "true" ) {
       $(this).attr("aria-checked", "false");
+      updateBigfoot(-1);
     } else {
       $(this).attr("aria-checked", "true");
+      updateBigfoot(1);
     }
   });
 });
+function updateBigfoot(num) {
+  var change = num;
+  var current = $(".happy-meter img").attr("src");
+  console.log(current);
+}

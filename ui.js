e7d26401a24cd3bf795e8c6f60817363afd1a286
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-05-13 16:27:51 -0400

    Update ui.js

--- ui.js
+++ ui.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
     var dest = $(this).attr("href");
     $('html,body').animate({scrollTop: $(dest).offset().top},'slow');
   });
+  $(".checkbox").click(function() {
+    if ($(this).attr(aria-checked) === "true" ) {
+      $(this).attr(aria-checked, "false");
+    } else {
+      $(this).attr(aria-checked, "true");
+    }
+  });
 });
